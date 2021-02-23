1d5122651844e5e187be479de9519fa599ca78d0
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-02-23 16:14:32 +0100

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import * as rezo from './rezo.js';
 
-rezo.search('fourchette', {relationTypes: ['r_has_part']})
-    .then(dump => console.log(dump.getInRelations()))
-    .catch(console.error);
+export default rezo.search;
